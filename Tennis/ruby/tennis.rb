10125d747261215da3ca2b371b4121889cac71c7
Victor Brender <victor@2xwide.com>
2016-09-21 10:18:14 +0200

    Spacing.

--- Tennis/ruby/tennis.rb
+++ Tennis/ruby/tennis.rb
@@ -120,29 +120,29 @@ class TennisGame3
   def initialize(p1_name, p2_name)
     @p1N = p1_name
     @p2N = p2_name
-    @p1 = 0
-    @p2 = 0
+    @p1  = 0
+    @p2  = 0
   end
       
   def won_point(n)
     if n == @p1N
-        @p1 += 1
+      @p1 += 1
     else
-        @p2 += 1
+      @p2 += 1
     end
   end
   
   def score
-    if (@p1 < 4 and @p2 < 4)
+    if @p1 < 4 && @p2 < 4
       p = ["Love", "Fifteen", "Thirty", "Forty"]
       s = p[@p1]
       @p1 == @p2 ? s + "-All" : s + "-" + p[@p2]
     else
-      if (@p1 == @p2)
+      if @p1 == @p2
         "Deuce"
       else
         s = @p1 > @p2 ? @p1N : @p2N
-        (@p1-@p2)*(@p1-@p2) == 1 ? "Advantage " + s : "Win for " + s
+        (@p1 - @p2) * (@p1 - @p2) == 1 ? "Advantage " + s : "Win for " + s
       end
     end
   end
