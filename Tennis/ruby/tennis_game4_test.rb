3305e52ece455ddb703663fe485c928f7d276917
Florin Oltean <florin@Florin-MBA.fritz.box>
2016-09-21 12:00:45 +0200

    Add next test case.

--- Tennis/ruby/tennis_game4_test.rb
+++ Tennis/ruby/tennis_game4_test.rb
@@ -6,4 +6,10 @@ class TestTennisGame4 < Test::Unit::TestCase
     game = TennisGame4.new(nil, nil)
     assert_equal('Love-All', game.score)
   end
+
+  def test_player_one_has_one_point_is_fifteen_love
+    game = TennisGame4.new('Name 1', 'Name 2')
+    game.won_point('Name 1')
+    assert_equal('Fifteen-Love', game.score)
+  end
 end
