8efcc5b5512268ed287c0af13a812c321cf659cc
Florin Oltean <florin@Florin-MBA.fritz.box>
2016-11-02 11:02:06 +0100

    Implement categories: yahtzee and ones.

--- Yahtzee/ruby/tdd_yahtzee.rb
+++ Yahtzee/ruby/tdd_yahtzee.rb
@@ -4,4 +4,16 @@ module Yahtzee
   def chance(roll)
     roll.reduce(:+)
   end
+
+  def yahtzee(roll)
+    roll.uniq.size == 1 ? 50 : 0
+  end
+
+  def ones(roll)
+    roll.select { |e| e == 1 }.reduce(0, :+)
+  end
+
+  def twos(roll)
+    0
+  end
 end
--- Yahtzee/ruby/test_tdd_yahtzee.rb
+++ Yahtzee/ruby/test_tdd_yahtzee.rb
@@ -13,4 +13,24 @@ class TddYahtzeeTest < Test::Unit::TestCase
   def test_chance_with_roll_66666
     assert Yahtzee.chance([6,6,6,6,6]) == 30
   end
+
+  def test_yahtzee_with_all_the_same
+    assert_equal 50, Yahtzee.yahtzee([2,2,2,2,2])
+  end
+
+  def test_yahtzee_with_at_least_one_different
+    assert_equal 0, Yahtzee.yahtzee([2,2,4,2,2])
+  end
+
+  def test_ones_with_no_ones
+    assert_equal 0, Yahtzee.ones([2,2,2,2,2])
+  end
+
+  def test_ones_with_three_ones
+    assert_equal 3, Yahtzee.ones([1,2,2,1,1])
+  end
+
+  def test_twos_with_no_twos
+    assert_equal 0, Yahtzee.twos([1,1,1,1,1])
+  end
 end
