b2d1c27c64942b0a819284b0733380b170464c26
Florin Oltean <florin555@gmail.com>
2016-11-10 10:30:45 +0100

    Write outline for Yahtzee game with UI.

---
+++ Yahtzee/ruby/yahtzee_game.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/env ruby
+
+puts 'Welcome to Yahtzee!'
+
+# x.times do
+#   each_player do
+#     play_round(player)
+      puts "Player 1's turn"
+      puts 'Press Enter to roll'
+
+      ARGF.readline
+
+      roll = [1,2,3,4,5]
+
+      puts "You rolled: #{roll.inspect}"
+
+      # choose what you keep
+      # roll
+      # choose what you keep
+      # roll
+      # choose category
+      # add roll points to total
+#   end
+# end
+
+# display results
