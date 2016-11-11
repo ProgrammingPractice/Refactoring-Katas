07f350e0f5729b2dc2b27d87f196b5a5d55aff6b
Florin Oltean <florin555@gmail.com>
2016-11-11 11:15:25 +0100

    Implement roll_one. Switch to Minitest to have stubbing.

--- Yahtzee/ruby/test_yahtzee_game.rb
+++ Yahtzee/ruby/test_yahtzee_game.rb
@@ -1,9 +1,23 @@
-require_relative 'yahtzee_game'
-require 'test/unit'
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
 
-class YahtzeeGameTest < Test::Unit::TestCase
+require_relative 'yahtzee_game'
+
+class YahtzeeGameTest < Minitest::Test
   def test_roll_returns_a_roll_of_five_dice
     game = YahtzeeGame.new
     assert_equal 5, game.roll.size
+  end
+
+  def test_roll_one_returns_a_valid_dice_value
+    game = YahtzeeGame.new
+    assert (1..6).include?(game.roll_one)
+  end
+
+  def test_roll_one_returns_a_random_dice
+    game = YahtzeeGame.new
+    game.stub(:rand, 4) do
+      assert_equal 5, game.roll_one
+    end
   end
 end
--- Yahtzee/ruby/yahtzee_game.rb
+++ Yahtzee/ruby/yahtzee_game.rb
@@ -2,4 +2,8 @@ class YahtzeeGame
   def roll
     [1,1,1,1,1]
   end
+
+  def roll_one
+    rand(6) + 1
+  end
 end
